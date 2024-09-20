af0ac2ea2dcdf318e5fcaa0bc3a6807023ff19d6
SUPRAKASH BALLAV <Suprakashballav1997@gmail.com>
2024-09-21 00:07:09 +0530

    new project

--- Top_Courses_Project_useEffect_hook/src/components/Navbar.js
+++ Top_Courses_Project_useEffect_hook/src/components/Navbar.js
@@ -3,11 +3,12 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-        <nav className='bg-bgDark py-4'>
-            <h1 className='text-3xl font-bold text-center text-white '>Top Courses</h1>
-        </nav>
+      <nav className="bg-bgDark py-4">
+        <h1 className="text-3xl font-bold text-center text-white">Top Courses</h1>
+      </nav>
     </div>
   )
 }
 
-export default Navbar;
+export default Navbar
+
